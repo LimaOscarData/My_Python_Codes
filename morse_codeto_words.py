8844b92e5ab68cfe8a74594a37265005bcde1015
LimaOscarData <leventogut01@gmail.com>
2022-01-08 00:25:59 +0300

    some explainations added.

--- morse_codeto_words.py
+++ morse_codeto_words.py
@@ -1,3 +1,8 @@
+# write a converter function.
+# when a morse code given it should convert the morse codes to word/words.it must show the gap/gaps.
+# if there are three blanks("   "), it is a gap between words.There are two or more words.
+# if there is only one gap(" "), it is only one word or letter.
+
 morse_code_dict = { 'A':'.-', 'B':'-...',
                     'C':'-.-.', 'D':'-..', 'E':'.',
                     'F':'..-.', 'G':'--.', 'H':'....',
@@ -13,6 +18,7 @@ morse_code_dict = { 'A':'.-', 'B':'-...',
                     '0':'-----', ', ':'--..--', '.':'.-.-.-',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
                     '(':'-.--.', ')':'-.--.-'}
+
 def morse_code(sentence):
     converted_word =""
 
@@ -27,6 +33,7 @@ def morse_code(sentence):
                             converted_word += k
             converted_word+=" "
 
+
     elif " " in sentence:
         my_splitted_sentence=sentence.split(" ")
         for my_letter in my_splitted_sentence:
@@ -34,6 +41,7 @@ def morse_code(sentence):
                 for k, v in morse_code_dict.items():
                     if v==my_letter:
                         converted_word+=k
+
     else:
         my_letter=sentence
         if my_letter in morse_code_dict.values():
@@ -42,6 +50,6 @@ def morse_code(sentence):
                     converted_word += k
 
     print(converted_word)
-morse_code(".... . -.--   .--- ..- -.. .") # two or more words, with a single gap between every word.
+morse_code(".... . -.--   .--- ..- -.. .   .--- ..- -.. .") # two or more words, with a single gap between every word.
 morse_code(".... . -.--") # single word
 morse_code('....') #single letter
